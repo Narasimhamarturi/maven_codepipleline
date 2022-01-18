d4ee42a25f13880cb393af3649772dfd5b1debb3
jenisakthi <44342960+jenisakthi@users.noreply.github.com>
2022-01-18 15:34:54 +0530

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo chmod +x /opt/tomcat/apache-tomcat-9.0.56/bin/./startup.sh
-sudo /opt/tomcat/apache-tomcat-9.0.56/bin/./startup.sh
+sudo chmod +x /opt/tomcat/bin/./startup.sh
+sudo /opt/tomcat/bin/./startup.sh
